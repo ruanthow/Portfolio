8909241f99e11cf52672120d16b62bda6583fdbb
ruanthow <ruan.thow3@gmail.com>
2023-07-03 22:00:36 -0300

    carroseul de projetos terminado

--- web/src/js/index.js
+++ web/src/js/index.js
@@ -1,6 +1,58 @@
 var sobreMimBox = document.querySelector(".sobreMimBox");
-
 var isOpenBox = false;
+var carroseul = document.querySelector(".carroseul-container");
+let indexCarroseul = 0;
+var btnCarroseulItem = document.querySelectorAll(".btn-carroseul-item");
+btnCarroseulItem[0].style.backgroundColor = "#ffffffd7";
+
+btnCarroseulItem[0].addEventListener('click', (btn)=>{
+    carroseul.scrollLeft = 0;
+    btnCarroseulItem[0].style.backgroundColor = "#ffffffd7";
+    btnCarroseulItem[1].style.backgroundColor = "#ffffff63";
+    btnCarroseulItem[2].style.backgroundColor = "#ffffff63";
+    indexCarroseul = 0
+})
+btnCarroseulItem[1].addEventListener('click', (btn)=>{
+    carroseul.scrollLeft = carroseul.offsetWidth;
+    btnCarroseulItem[0].style.backgroundColor = "#ffffff63";
+    btnCarroseulItem[1].style.backgroundColor = "#ffffffd7";
+    btnCarroseulItem[2].style.backgroundColor = "#ffffff63";
+    indexCarroseul = 1
+})
+btnCarroseulItem[2].addEventListener('click', (btn)=>{
+    carroseul.scrollLeft = carroseul.offsetWidth * 2
+    btnCarroseulItem[0].style.backgroundColor = "#ffffff63";
+    btnCarroseulItem[1].style.backgroundColor = "#ffffff63";
+    btnCarroseulItem[2].style.backgroundColor = "#ffffffd7";
+    indexCarroseul = 2
+})
+
+setInterval(()=>{
+    
+    if(indexCarroseul == 0){
+        carroseul.scrollLeft = 0;
+        btnCarroseulItem[0].style.backgroundColor = "#ffffffd7";
+        btnCarroseulItem[1].style.backgroundColor = "#ffffff63";
+        btnCarroseulItem[2].style.backgroundColor = "#ffffff63";
+        indexCarroseul++;
+    }
+    else if(indexCarroseul == 1){
+        carroseul.scrollLeft = carroseul.offsetWidth;
+        btnCarroseulItem[0].style.backgroundColor = "#ffffff63";
+        btnCarroseulItem[1].style.backgroundColor = "#ffffffd7";
+        btnCarroseulItem[2].style.backgroundColor = "#ffffff63";
+        indexCarroseul++
+    }
+    else{
+        carroseul.scrollLeft = carroseul.offsetWidth * 2
+        btnCarroseulItem[0].style.backgroundColor = "#ffffff63";
+        btnCarroseulItem[1].style.backgroundColor = "#ffffff63";
+        btnCarroseulItem[2].style.backgroundColor = "#ffffffd7";
+        indexCarroseul = 0;
+    }
+   
+},5000)
+
 
 function hideBoxMe(){
     if(isOpenBox){
@@ -12,3 +64,4 @@ function hideBoxMe(){
         isOpenBox = true;
     }
 }
+
